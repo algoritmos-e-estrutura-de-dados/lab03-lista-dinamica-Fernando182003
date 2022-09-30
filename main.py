76c62e538fc566668c0e0484ebbb6a44e792d7bf
fernando godoy <fernando.godoy18@edu.unifil.br>
2022-09-30 16:14:16 -0300

    lista

---
+++ main.py
@@ -0,0 +1,16 @@
+from lista import Lista
+
+lista = Lista()
+
+lista.adicionar(1)
+lista.adicionar(2)
+lista.adicionar(3)
+lista.adicionar(4)
+lista.adicionar(5)
+lista.adicionar(6)
+
+lista.show()
+
+lista.remover_um(3)
+
+lista.show()
